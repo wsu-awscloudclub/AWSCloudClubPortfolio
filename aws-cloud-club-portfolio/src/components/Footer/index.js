7e148f6dc1cb29bc8cf2cade782f813a75a8c243
syedaahmed05 <161238906+syedaahmed05@users.noreply.github.com>
2024-03-08 15:38:50 -0500

    Created a basic outline for the footer.

--- aws-cloud-club-portfolio/src/components/Footer/index.js
+++ aws-cloud-club-portfolio/src/components/Footer/index.js
@@ -2,7 +2,41 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
+    <div className = "main-footer">
+      <div className = "container">
+        <div className = "row">
+          {/* Column1 */}
+          <div className = "col">
+              <h4>column1</h4>
+              <ul className = "list-unstyled">
+                <li></li>
+              </ul>
+          </div>
+          {/*Column2 */}
+          <div className = "column">
+              <h4>column2</h4>
+              <ul className = "list-unstyled">
+                <li></li>
+              </ul>
+
+          </div>
+          {/*Column3*/}
+          <div className = "column">
+              <h4>column3</h4>
+              <ul className = "list-unstyled">
+                <li></li>
+              </ul>
+
+          </div>
+
+        </div>
+        <hr />
+        <div className = "row">
+          <p className = "col-sm">
+            &copy;{new Date().getFullYear()} Wayne State University AWS Cloud Club | All rights reserved
+          </p>
+        </div>
+      </div>
       
     </div>
   )
