5739d1de5eb432d742aec7fd926dfdada0840f4f
syedaahmed05 <161238906+syedaahmed05@users.noreply.github.com>
2024-03-16 14:51:30 -0400

    copied Ayman's design for footer

--- aws-cloud-club-portfolio/src/components/Footer/index.js
+++ aws-cloud-club-portfolio/src/components/Footer/index.js
@@ -1,45 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faFacebook, faInstagram, faDiscord, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import './Footer.css'; 
 
-const Footer = () => {
+function Footer() {
   return (
-    <div className = "main-footer">
-      <div className = "container">
-        <div className = "row">
-          {/* Column1 */}
-          <div className = "col">
-              <h4>column1</h4>
-              <ul className = "list-unstyled">
-                <li></li>
-              </ul>
-          </div>
-          {/*Column2 */}
-          <div className = "column">
-              <h4>column2</h4>
-              <ul className = "list-unstyled">
-                <li></li>
-              </ul>
-
-          </div>
-          {/*Column3*/}
-          <div className = "column">
-              <h4>column3</h4>
-              <ul className = "list-unstyled">
-                <li></li>
-              </ul>
-
-          </div>
-
-        </div>
-        <hr />
-        <div className = "row">
-          <p className = "col-sm">
-            &copy;{new Date().getFullYear()} Wayne State University AWS Cloud Club | All rights reserved
-          </p>
-        </div>
+    <footer className="footer">
+      <div className="social-icons">
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faTwitter} className="icon" />
+        </a>
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faFacebook} className="icon" />
+        </a>
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faInstagram} className="icon" />
+        </a>
+        <a href="https://discord.com" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faDiscord} className="icon" />
+        </a>
+        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} className="icon" />
+        </a>
       </div>
-      
-    </div>
-  )
+      <div className="footer-links">
+        <a href="/about">About Us</a>
+        <a href="/contact">Contact Us</a>
+        <a href="/join_us">Become a Member</a>
+      </div>
+      <div className="newsletter-signup">
+        <p>Subscribe to our newsletter for updates:</p>
+        <input type="email" placeholder="Enter your email" />
+        <button>Subscribe</button>
+      </div>
+      <div className="logo-container">
+          <img src="https://media.licdn.com/dms/image/D560BAQGV-F31g-N5Cg/company-logo_200_200/0/1696183988983?e=1718236800&v=beta&t=DKGiWuAI1AW4SLDgIQSteb6k3wXLoOkjedc6kJ22VYs" alt="Club Logo" className="logo" />
+          <img src="https://d3sxshmncs10te.cloudfront.net/icon/premium/png-512/4488889.png?token=eyJhbGciOiJoczI1NiIsImtpZCI6ImRlZmF1bHQifQ__.eyJpc3MiOiJkM3N4c2htbmNzMTB0ZS5jbG91ZGZyb250Lm5ldCIsImV4cCI6MTcxMDY1MzExNiwicSI6bnVsbCwiaWF0IjoxNzEwMzkzOTE2fQ__.3c806d4076bb795f58bd767997febd05b23c851eaf5bd385615a13398f71f382" alt="AWS Logo" className="logo" />
+          <img src="https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-1/302495982_488615826604779_4341638918318599601_n.png?stp=dst-png_p320x320&_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=rHbPi8QJLY0AX87Drt8&_nc_ht=scontent-ord5-2.xx&oh=00_AfAWj5E46OOE-jl4bNyjxuEoiXu5tYnJPiD1ZWZBqYUPbA&oe=65F7F8BB" alt="WSU Logo " className="logo" />
+        </div>
+         <p className="copyright">© 2024 Wayne State AWS Cloud Club</p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
