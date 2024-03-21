e4d239b2ca9688329801384004ec3043bc067c8f
AaronMorriso <aaronmorrison03@gmail.com>
2024-03-20 21:13:40 -0400

    Code from Hero Section video
    I copied directly from the given video starting at 59:00 and there is an error due to the './Button' import

--- aws-cloud-club-portfolio/src/components/HeroSection/HeroSection.js
+++ aws-cloud-club-portfolio/src/components/HeroSection/HeroSection.js
@@ -1,1 +1,51 @@
 /*HI*/
+
+import React from "react";
+import {Button} from './Button'
+import {Link} from 'react-dom'
+
+function HeroSection({
+    lightBg, topLine, lightText, lightTextDessc, headline, description, buttonLabel,
+    img, alt, imgStart
+}) {
+    return (
+    <>
+        <div 
+            className={lightBg ? 'home__her-section' : 'home__hero-section darkBg'}
+        >    
+        <div className="container">
+            <div className="row home__hero-row">
+                style={{display: 'flex', flexDirection: imgStart === 'start' ? 'row-reverse' : 'row'}}
+                
+                <div classname="col">
+                    <div className='home__hero-text-wrapper'>
+                        <div className="top-line">{topline}</div>
+                        <h1 className={lightText ? 'heading' : 'heading dark'}>{headline}</h1>
+                        <p className={lightTextDesc 
+                            ? 'home__hero-subtitle' 
+                            : 'home__hero-subtitle dark'
+                            }
+                        >
+                            {description}
+                        </p>
+                        <Link to="/sign-up">
+                            <Button buttonSize='btn--wide' buttonColor='blue'>
+                                {buttonLabel}
+                            </Button>
+                        </Link>
+                    </div>    
+                </div>
+                <div className="col">
+                    <div className="home__hero-img-wrapper">
+                        <img src={img} alt={alt} className="home__hero-img"/>
+                    </div>
+                </div>
+            </div>    
+        </div>
+    </div>
+    </>
+    );
+}
+
+
+export default HeroSection;
