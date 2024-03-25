a17cf0e694f4ce2ad6aa8ecbf745addfff2e64bc
MdKhalique <64277862+MdKhalique@users.noreply.github.com>
2024-03-25 16:41:36 -0400

    First Iteration
    Generic Description

--- aws-cloud-club-portfolio/src/components/Footer/index.js
+++ aws-cloud-club-portfolio/src/components/Footer/index.js
@@ -1,9 +1,27 @@
-import React from 'react'
+/*
+
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faFacebook, faInstagram, faDiscord, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import './Footer.css'; 
 
 const Footer = () => {
   return (
     <div>
       
+    </div>
+  )
+}
+
+export default Footer;
+
+*/
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <h1>Footer Section Goes Here</h1>
     </div>
   )
 }
