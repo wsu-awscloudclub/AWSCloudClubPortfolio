6be409b57c225857758a06353002e1bd8c3c43cc
Talha Ali <waynestatecloud@gmail.com>
2024-03-28 21:58:49 -0400

    updated about section

--- aws-cloud-club-portfolio/src/components/AboutSection/index.js
+++ aws-cloud-club-portfolio/src/components/AboutSection/index.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import './AboutSection.css';
 
 const AboutSection = () => {
   return (
-    <div>
-      <h1>About Section Goes Here</h1>
+    <div className="about-section">
+      <div className="container">
+        <h1>About Us</h1>
+        <p>Welcome to the AWS Cloud Club at Wayne State University!</p>
+        <p>
+          We are a group of passionate individuals dedicated to exploring,
+          learning, and sharing knowledge about Amazon Web Services (AWS) and
+          cloud computing technologies.
+        </p>
+        <p>
+          Whether you're a beginner or an experienced cloud enthusiast, our
+          club offers workshops, events, and resources to help you expand your
+          skills and connect with like-minded individuals.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutSection
+export default AboutSection;
